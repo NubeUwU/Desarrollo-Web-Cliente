1cae328724231083aa10bab01f342645b399e63e
Marcos Magadán Rodríguez <Leapbs2017@gmail.com>
2025-09-30 13:39:57 +0200

    Ejemplo

---
+++ U2_Sintaxis_Basica_JavaScript/00_Pruebas/test.js
@@ -0,0 +1,11 @@
+/**
+ * @summary Calcula la suma de dos números
+ * @description Esta función sirve para sumar dos números que toma como parámetro
+ * @param {number} a Primer sumando
+ * @param {number} b Segundo sumando
+ * @returns {number} Devuelve la suma de los dos números
+ */
+
+function suma(a , b){
+    return a + b;
+}
