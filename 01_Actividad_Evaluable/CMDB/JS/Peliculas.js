3e6d4f6a63249d93283b3f4c5708e0ee36a52501
Nube <Leapbs2017@gmail.com>
2025-12-02 10:35:28 +0100

    #

--- 01_Actividad_Evaluable/CMDB/JS/Peliculas.js
+++ 01_Actividad_Evaluable/CMDB/JS/Peliculas.js
@@ -1,3 +1,31 @@
+// Clase Generos
+class Genero {
+  static ultimoId = 0;
+
+  constructor(nombre) {
+    this._id = ++Genero.ultimoId; 
+    this.nombre = nombre;          
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set nombre(valor) {
+    if (typeof valor !== "string" || valor.length > 100) {
+      throw new Error("Nombre de género inválido");
+    }
+    this._nombre = valor;
+  }
+
+  get nombre() {
+    return this._nombre;
+  }
+}
+
+
+
+
 // Clase Pelicula
 class Pelicula {
   static ultimoId = 0;
--- U5_React/Pruebas/hola-mundo/src/App.jsx
+++ U5_React/Pruebas/hola-mundo/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { useState } from "react";
+function App() { 
+  const [estado, setEstado] = useState({ 
+    titulo: "Por defecto", 
+    hora: new Date().toLocaleTimeString(), 
+    numero: 0, 
+    numeros: [], 
+  }); 
+  const cambiarEstado = () => { 
+    let numero = Math.round(Math.random() * 4); 
+    let numeros = estado.numeros; 
+    numeros.push(numero); 
+    setEstado({ 
+      hora: new Date().toLocaleTimeString(), 
+      numeros: numeros, 
+      numero: numero, 
+      titulo: numero % 2 === 0 ? "Número par" : "Número impar", 
+    }); 
+    console.log("cambiarEstado> ", estado); 
+  }; 
+   
+  const colores = ["red", "yellow", "green", "blue", "orange"]; 
+  
+  return ( 
+    <div className="App" style={{ backgroundColor: colores[estado.numero] }}> 
+      <header> 
+        <h1> 
+          {estado.titulo} - {estado.numero} 
+        </h1> 
+      </header> 
+      <div className="App-intro"> 
+        <div>{estado.hora}</div> 
+        Pulsa el botón para cambiar el estado 
+        <div> 
+          <button onClick={cambiarEstado}>Cambiar estado</button> 
+        </div> 
+        <div> 
+          Números generados: 
+          <ul> 
+            {estado.numeros.map((n) => ( 
+              <li key={n}>{n}</li> 
+            ))} 
+          </ul> 
+        </div> 
+      </div> 
+    </div> 
+  ); 
 }
 
 export default App
